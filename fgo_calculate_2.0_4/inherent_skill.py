540e25620ca2ae8cc8ae44d560b27db9d9dd3466
hzk <705248010@qq.com>
2020-09-10 19:46:04 +0800

    2.0.4

---
+++ fgo_calculate_2.0_4/inherent_skill.py
@@ -0,0 +1,3 @@
+inherent_skill_list = ['Buster', 'Arts', 'Quick', '特攻', '伤害', '宝具威力']
+
+# 只计算宝具伤害，暴击buff不计入
---
+++ fgo_calculate_2.0_4/select_servant.py
@@ -0,0 +1,132 @@
+from np_effect_of_pre import *
+from inherent_skill import *
+
+
+# 改一下宝具倍率的函数，改成传参（宝具等级，然后返回倍率）
+class SelectServant:
+    def __init__(self, servant_np_info, servant_atk_info, servant_level, rarity, _class, name, inherent_skill_info):
+        self.servant_np_info = servant_np_info
+        self.servant_level = servant_level
+        self.servant_atk_info = servant_atk_info
+        self.rarity = rarity
+        self._class = _class
+        self.name = name
+        self.inherent_skill_info = inherent_skill_info
+
+    # 根据宝具卡色和宝具等级，进行宝具倍率等值计算
+    def np_multiplying_power(self, np_level):
+        if '马修' in self.name:
+            return ''
+        if self.servant_np_info[0][0] == 'Arts':
+            if self.servant_np_info[0][1] == '单体':
+                return self.get_np_level_info()[np_level]
+            elif self.servant_np_info[0][1] == '全体':
+                return self.get_np_level_info()[np_level]
+            else:
+                pass
+        elif self.servant_np_info[0][0] == 'Buster':
+            if self.servant_np_info[0][1] == '单体':
+                return self.get_np_level_info()[np_level]
+            elif self.servant_np_info[0][1] == '全体':
+                return self.get_np_level_info()[np_level]
+            else:
+                pass
+        elif self.servant_np_info[0][0] == 'Quick':
+            if self.servant_np_info[0][1] == '单体':
+                return self.get_np_level_info()[np_level]
+            elif self.servant_np_info[0][1] == '全体':
+                return self.get_np_level_info()[np_level]
+            else:
+                pass
+
+    # 对宝具前置效果进行计算
+    def np_effect_of_pre(self):
+        if '玛修·基列莱特' == self.name:
+            return ''
+        np_effect_of_pre = {}
+        for i in range(len(self.servant_np_info)):
+            for effect in effect_of_pre:
+                if effect in self.servant_np_info[i][3] or '强大' in self.servant_np_info[i][3]:
+                    if '强大' in self.servant_np_info[i][3]:
+                        break
+                    np_effect_of_pre[self.servant_np_info[i][3]] = self.servant_np_info[i][4]
+            if '强大' in self.servant_np_info[i][3]:
+                break
+        return np_effect_of_pre
+
+    # 根据传入的从者等级，计算出atk
+    def atk_transform(self):
+        if '玛修·基列莱特' == self.name:
+            return self.servant_atk_info[79]
+        if self.servant_level == '满破':
+            if self.rarity == '1':
+                return self.servant_atk_info[59]
+            elif self.rarity == '2':
+                return self.servant_atk_info[64]
+            elif self.rarity == '3':
+                return self.servant_atk_info[69]
+            elif self.rarity == '4':
+                return self.servant_atk_info[79]
+            elif self.rarity == '5':
+                return self.servant_atk_info[89]
+        elif self.servant_level == '90级':
+            return self.servant_atk_info[89]
+        elif self.servant_level == '100级':
+            return self.servant_atk_info[99]
+
+    # 返回计算结果
+    def get_calculate_result(self):
+        atk = self.atk_transform()
+        effect = self.np_effect_of_pre()
+        return atk, effect
+
+    # 返回宝具等级对应的倍率
+    def get_np_level_info(self):
+        for i in range(len(self.servant_np_info)):
+            if '强大' in self.servant_np_info[i][3]:
+                np_level_info = {
+                    '1': self.servant_np_info[i][4],
+                    '2': self.servant_np_info[i][5],
+                    '3': self.servant_np_info[i][6],
+                    '4': self.servant_np_info[i][7],
+                    '5': self.servant_np_info[i][8],
+                }
+                return np_level_info
+
+    # 返回职阶补正
+    def get_class_additions(self):
+        if self._class == 'Saber':
+            return '1'
+        elif self._class == 'Lancer':
+            return '1.05'
+        elif self._class == 'Archer':
+            return '0.95'
+        elif self._class == 'Rider':
+            return '1'
+        elif self._class == 'Caster':
+            return '0.9'
+        elif self._class == 'Assassin':
+            return '0.9'
+        elif self._class == 'Berserker':
+            return '1.1'
+        elif self._class == 'Ruler':
+            return '1.1'
+        elif self._class == 'Avenger':
+            return '1.1'
+        elif self._class == 'Mooncancer':
+            return '1'
+        elif self._class == 'Alterego':
+            return '1'
+        elif self._class == 'Foreigner':
+            return '1'
+        elif self._class == 'Shielder':
+            return '1'
+
+    # 计算职阶技能
+    def inherent_skill(self):
+        inherent_skill = []
+        for effect in self.inherent_skill_info:
+            for skill in inherent_skill_list:
+                if skill in effect:
+                    inherent_skill.append(effect)
+        return inherent_skill
